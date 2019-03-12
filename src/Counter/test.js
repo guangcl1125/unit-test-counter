60ec166c9067bd6d843a343d708fd28ad4a94064
LIU GUANG CHEN <boyislight@gmail.com>
2019-03-12 10:38:19 +0800

    setup test js & counter test stript

---
+++ src/Counter/test.js
@@ -0,0 +1,16 @@
+describe('測試一組規範 ', () => {
+    it('測試規格1', () => {
+        expect(true).toBeTruthy()
+    })
+
+    it('測試規格2', () => {
+        expect(1).toBe(1)
+    })
+
+    it('測試規格3', () => {
+        expect('happy').toBe('happy')
+    })
+
+})
+
+
