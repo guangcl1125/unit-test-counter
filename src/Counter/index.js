5a0d7e756549b8a43cb9b8834fb15c843cc0d39c
LIU GUANG CHEN <boyislight@gmail.com>
2019-03-12 10:16:53 +0800

    add Counter component

---
+++ src/Counter/index.js
@@ -0,0 +1,30 @@
+import React, { Component } from 'react';
+import './index.css';
+class Counter extends Component {
+  constructor(props) {
+    super(props);
+    this.state = {
+      counter: 0,
+    };
+  }
+
+  handleIncreaseOne = () => {
+    this.setState({ counter: this.state.counter + 1 });
+  };
+
+  handleDecreaseOne = () => {
+    this.setState({ counter: this.state.counter - 1 });
+  };
+
+  render() {
+    const { counter } = this.state;
+    return (
+      <div className="counter-page">
+        <h1 className="count">counter: { counter }</h1>
+        <button onClick={this.handleIncreaseOne}> + 1 </button>
+        <button onClick={this.handleDecreaseOne}> - 1 </button>
+      </div>
+    );
+  }
+}
+export default Counter;
